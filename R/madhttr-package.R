864e9a1e11fe7293f88927ef4c634d81be24accb
hrbrmstr <bob@rud.is>
2019-07-08 12:55:46 -0400

    R package repo initialization complete

---
+++ R/madhttr-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/madhttr>
+#' - BugReports: <https://gitlab.com/hrbrmstr/madhttr/issues>
+#' 
+#' @md
+#' @name madhttr
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+"_PACKAGE"
